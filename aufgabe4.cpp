dd712ddb29dce77984ee03e5daf68c643fa0c786
Jonas Langlotz <jonas.l1512@gmail.com>
2021-02-27 13:46:26 +0100

    Commented out deliberate error instead of letting it fail

--- aufgabe4.cpp
+++ aufgabe4.cpp
@@ -5,5 +5,5 @@ int main() {
     // Kreis kann weiter erstellt werden
     Kreis kreis = Kreis(5);
     // Graph kann einzeln nicht erstellt werden
-    Graph graph = Graph(5, 20);
+    // Graph graph = Graph(5, 20);
 }
